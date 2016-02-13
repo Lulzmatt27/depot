5a256fd7afaa1ceb82496421155cc67a1ad18484
Lulzmatt27 <lulmatt27@gmail.com>
2016-02-13 19:04:31 +0500

    session
    add count and unless count

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
+  def coun_t
+  	if session[:counter].nil?
+  		session[:counter]=0
+  	end
+  	session[:counter]+=1
+  end
+
   def index
   	@products = Product.order(:title)
+  	
+  	@counter  = coun_t
+ @shown_message = "You've been here #{@counter} times" unless @counter <5
   end
 end
